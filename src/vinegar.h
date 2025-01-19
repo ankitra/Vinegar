9fc7b11de6e1ae2d48a4b25d3b4325125799c8d0
Ankit Raizada <17532455+ankitra@users.noreply.github.com>
2025-01-18 21:48:37 -0800

    copy paste error

--- src/vinegar.h
+++ src/vinegar.h
@@ -5,7 +5,7 @@
 #define CHECKED_CUDA_API(api) { \
     cudaError_t err = api; \
     if (err != cudaSuccess) { \
-    printf(“%s in %s at line %d\n”, cudaGetErrorString(err), __FILE__, __LINE__); \
+    printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); \
     exit(EXIT_FAILURE); \
    } \
 }
