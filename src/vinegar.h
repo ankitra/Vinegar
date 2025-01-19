81a5993ede3707cd913ad332c94cbd792949b3af
Ankit Raizada <17532455+ankitra@users.noreply.github.com>
2025-01-18 21:45:18 -0800

    fixed typo

--- src/vinegar.h
+++ src/vinegar.h
@@ -4,7 +4,7 @@
 
 #define CHECKED_CUDA_API(api) { \
     cudaError_t err = api; \
-    if (error != cudaSuccess) { \
+    if (err != cudaSuccess) { \
     printf(“%s in %s at line %d\n”, cudaGetErrorString(err), __FILE__, __LINE__); \
     exit(EXIT_FAILURE); \
    } \
