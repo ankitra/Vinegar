87e3dc3441a9b829d6f0d73110657f1e5da0dee5
Ankit Raizada <17532455+ankitra@users.noreply.github.com>
2025-01-21 16:12:01 -0800

    Lets gooo

---
+++ letsgooo.sh
@@ -0,0 +1,7 @@
+touch ~/.no_auto_tmux
+pip install --upgrade pip
+pip install nvitop
+pip install "sglang[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
+sudo apt-get install python3-dev
+python3 -m sglang.launch_server --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --trust-remote-code --tp 2
+
