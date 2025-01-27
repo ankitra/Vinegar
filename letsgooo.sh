9c4eee045269554f04753021080ac8b3780c7fbb
Ankit Raizada <17532455+ankitra@users.noreply.github.com>
2025-01-27 09:10:52 -0800

    Lets goooo

--- letsgooo.sh
+++ letsgooo.sh
@@ -3,5 +3,5 @@ pip install --upgrade pip
 pip install nvitop
 pip install "sglang[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
 sudo apt-get -y install python3-dev
-python3 -m sglang.launch_server --model deepseek-ai/DeepSeek-R1-Distill-Qwen-7B --trust-remote-code --tp 2
+python3 -m sglang.launch_server --model deepseek-ai/DeepSeek-R1 --trust-remote-code --tp 1
 
