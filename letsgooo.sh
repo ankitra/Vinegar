5ce720937ae9fadd41f616bc01d97ba5e8f51034
Ankit Raizada <17532455+ankitra@users.noreply.github.com>
2025-01-28 01:11:18 -0800

    lets go

--- letsgooo.sh
+++ letsgooo.sh
@@ -1,4 +1,6 @@
 touch ~/.no_auto_tmux
+sudo apt-get install python3-dev
+curl https://bootstrap.pypa.io/get-pip.py | python3
 pip install --upgrade pip
 pip install nvitop
 pip install "sglang[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
